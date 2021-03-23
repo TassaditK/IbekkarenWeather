0f3a5091f6f07a54dbd4c113d94300eb1376fe8a
TassaditK <77680024+TassaditK@users.noreply.github.com>
2021-03-23 22:40:47 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let url_parser = require('url')
+let http = require('http')
+let fs =require("fs");
+let mustache = require('mustache');
+let request = require('request');
+let url2;
+let result;
+let city;
+let html_doc = fs.readFileSync('./index.html').toString();
+
+let server = http.createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/html'});
+    let url = url_parser.parse(req.url, true);
+    city = url.query.city;
+    url2 = 'https://api.openweathermap.org/data/2.5/weather?q=Ibekkaren&appid=620d84604500582ade540ab962c47c4a&units=metric&lang=fr';
+    request(url2, (err, res2, body) => {
+        let data = JSON.parse(body);
+        result = mustache.render(html_doc,data);
+        res.end(result);
+    }); 
+});
+server.listen(3000);
+
+
+
